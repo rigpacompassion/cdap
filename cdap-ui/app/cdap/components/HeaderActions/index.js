d57f2111d5f65cc2c55047e5484ca7f44bd97537
Ajai <ajainarayanan@gmail.com>
2016-10-07 18:22:53 -0700

    Adds default namespace + styling for no namespace dropdown

--- cdap-ui/app/cdap/components/HeaderActions/index.js
+++ cdap-ui/app/cdap/components/HeaderActions/index.js
@@ -42,7 +42,6 @@ export default class HeaderActions extends Component {
     this.namespaceMap = '';
     this.toggleNamespaceDropdown = this.toggleNamespaceDropdown.bind(this);
     this.loadNamespacesFromStore = this.loadNamespacesFromStore.bind(this);
-    // this.selectNamespace = this.selectNamespace.bind(this);
   }
 
   componentWillMount(){
@@ -50,7 +49,7 @@ export default class HeaderActions extends Component {
     //Load the namespaces into the dropdown from the store
     this.namespaceList = Store.getState().namespaces;
     if(this.namespaceList){
-      this.loadNamespacesFromStore(this.namespaceList, Store.getState().selectedNamespace);
+      this.loadNamespacesFromStore(this.namespaceList);
     }
 
     //Load updated data into the namespace dropdown
@@ -99,30 +98,32 @@ export default class HeaderActions extends Component {
       return;
     }
 
-    this.namespaceMap = namespaceList.map( (item) => {
-
-      //If there is no current namespace, set it to default if possible
-      if(!this.state.currentNamespace && item.name === 'default'){
-        this.defaultNamespace = item.name;
-      }
-
-      let checkClass = classNames({ "fa fa-star": currentNamespace === item.name });
-      let check = <span className={checkClass}></span>;
-
-      return (
+    this.namespaceMap = namespaceList
+      .filter(item => item.name !== currentNamespace)
+      .map( (item) => {
+        let defaultNamespace = localStorage.getItem('DefaultNamespace');
+        let checkClass = classNames({ "fa fa-star":  defaultNamespace === item.name });
+        let check = <span className={checkClass}></span>;
+        return (
           <Link to={`/ns/${item.name}`} key={shortid.generate()}>
             <div
-              className="clearfix"
+              className="clearfix namespace-container"
               onClick={this.selectNamespace.bind(this, item.name)}>
               <span className="namespace-name pull-left">{item.name}</span>
-              <span className="pull-right">{check}</span>
+              <span className="default-ns-section pull-right">
+                {check}
+                <span className="default-btn">
+                  <span
+                    className="btn btn-default btn-xs"
+                    onClick={() => localStorage.setItem('DefaultNamespace', item.name)}>
+                    Default
+                  </span>
+                </span>
+              </span>
             </div>
           </Link>
-      );
-    });
-    this.namespaceMap = [...this.namespaceMap, (
-      <div className="namespace-action text-center"> Manage Namespaces </div>
-    )];
+        );
+      });
   }
 
   selectNamespace(name){
@@ -224,6 +225,7 @@ export default class HeaderActions extends Component {
             <Dropdown
               isOpen={this.state.namespaceOpen}
               toggle={this.toggleNamespaceDropdown}
+              className={classNames({'no-namespace': this.namespaceMap.length === 0})}
             >
               <div
                 className="current-namespace"
@@ -236,7 +238,23 @@ export default class HeaderActions extends Component {
                 </span>
               </div>
               <DropdownMenu>
-                {this.namespaceMap}
+                <div className="namespace-list">
+                  {this.namespaceMap}
+                </div>
+                {
+                  this.namespaceMap.length > 0 ?
+                    (<div className="namespace-action text-center">
+                      Manage Namespaces
+                    </div>)
+                  :
+                    (
+                      <div className="namespace-action text-center">
+                        Add Namespace
+                      </div>
+                    )
+
+
+                }
               </DropdownMenu>
             </Dropdown>
           </div>
